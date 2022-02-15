1ebc2100bcf5cadd2f8bffac263a58c70762718c
Ahmed Taha <ahmed.taha8399@gmail.com>
2022-02-16 01:18:55 +0200

    First commit

---
+++ Tour Guide/Details.swift
@@ -0,0 +1,16 @@
+//
+//  Details.swift
+//  Tour Guide
+//
+//  Created by Ahmed Taha on 15/02/2022.
+//
+
+import Foundation
+
+struct Details {
+    
+    let placeName: String
+    let image: String
+    let description: String
+    
+}
---
+++ Tour GuideTests/Tour_GuideTests.swift
@@ -0,0 +1,33 @@
+//
+//  Tour_GuideTests.swift
+//  Tour GuideTests
+//
+//  Created by Ahmed Taha on 15/02/2022.
+//
+
+import XCTest
+@testable import Tour_Guide
+
+class Tour_GuideTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
